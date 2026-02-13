c4ded94a05eaf7b8776f8dcc1b9b829e10c494fb
diosaurr <batonxleba1239@gmail.com>
2026-02-13 23:12:57 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+const envelope = document.getElementById("envelope-cont");
+const letter = document.getElementById("letter-cont");
+const noBtn = document.querySelector(".no-btn");
+const yesBtn = document.querySelector(".yes-btn");
+
+const title = document.getElementById("letter-lit");
+const catImg = document.getElementById("cat-letter");
+const buttons = document.getElementById("letter-buttons");
+const finalText = document.querySelector(".final-text");
+
+
+
+envelope.addEventListener("click", () => {
+    envelope.style.display = "none";
+    letter.style.display = "flex";
+
+    setTimeout( () => {
+        document.querySelector(".letter-window").classList.add("open");
+    },50);
+});
+
+
+noBtn.addEventListener("mouseover", () => {
+    const min = 200;
+    const max = 200;
+
+    const distance = Math.random() * (max - min) + min;
+    const angle = Math.random() * Math.PI * 2;
+
+    const moveX = Math.cos(angle) * distance;
+    const moveY = Math.sin(angle) * distance;
+
+    noBtn.style.transition = "transform 0.3s ease";
+    noBtn.style.transform = `translate(${moveX}px, ${moveY}px)`;
+});
+
+yesBtn.addEventListener("click", () => {
+    title.textContent = "Je t'aime, chérie!";
+
+    catImg.src = "cat_dance.gif";
+
+    document.querySelector(".letter-window").classList.add("final");
+
+    buttons.style.display = "none";
+
+    finalText.style.display = "block";
+});
